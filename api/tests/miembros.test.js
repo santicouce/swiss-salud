10818e9bfa152b91554a831513c5416325870d34
Santiago <santicouce@outlook.com>
2024-05-20 00:01:48 -0300

    actualizo tests para que se cierre el servidor al terminar

--- api/tests/miembros.test.js
+++ api/tests/miembros.test.js
@@ -3,55 +3,55 @@ const app = require('../app');
 
 describe('GET /miembros', () => {
   it('debería devolver una lista de miembros', async () => {
-    const response = await request(app)
-      .get('/miembros')
-      .expect(200);
-
-    expect(response.body).toEqual([
+    const expectedMembers = [
       {
-        id: 1,
-        nombre: 'Juan',
-        apellido: 'Perez',
-        fechaNacimiento: '01-01-2000',
-        familiaId: 1,
-        inicioCobertura: '01-01-2024',
-        finCobertura: '01-01-2025',
-        planId: 1,
-        onboarding: 'completo'
+        "id": 1,
+        "nombre": "Juan",
+        "apellido": "Perez",
+        "fechaNacimiento": "01-01-2000",
+        "familiaId": 1,
+        "inicioCobertura": "01-01-2024",
+        "finCobertura": "01-01-2025",
+        "planId": 1,
+        "onboarding": "completo"
       },
       {
-        id: 2,
-        nombre: 'Maria',
-        apellido: 'Perez',
-        fechaNacimiento: '01-01-2000',
-        familiaId: 1,
-        inicioCobertura: '01-01-2024',
-        finCobertura: '01-01-2025',
-        planId: 1,
-        onboarding: 'completo'
+        "id": 2,
+        "nombre": "Maria",
+        "apellido": "Perez",
+        "fechaNacimiento": "01-01-2000",
+        "familiaId": 1,
+        "inicioCobertura": "01-01-2024",
+        "finCobertura": "01-01-2025",
+        "planId": 1,
+        "onboarding": "completo"
       },
       {
-        id: 3,
-        nombre: 'Juan',
-        apellido: 'Couce',
-        fechaNacimiento: '01-01-2000',
-        familiaId: 2,
-        inicioCobertura: '01-01-2024',
-        finCobertura: '01-01-2025',
-        planId: 2,
-        onboarding: 'completo'
+        "id": 3,
+        "nombre": "Juan",
+        "apellido": "Couce",
+        "fechaNacimiento": "01-01-2000",
+        "familiaId": 2,
+        "inicioCobertura": "01-01-2024",
+        "finCobertura": "01-01-2025",
+        "planId": 2,
+        "onboarding": "completo"
       },
       {
-        id: 4,
-        nombre: 'Maria',
-        apellido: 'Couce',
-        fechaNacimiento: '01-01-2000',
-        familiaId: 2,
-        inicioCobertura: '01-01-2024',
-        finCobertura: '01-01-2025',
-        planId: null,
-        onboarding: 'en progreso'
+        "id": 4,
+        "nombre": "Maria",
+        "apellido": "Couce",
+        "fechaNacimiento": "01-01-2000",
+        "familiaId": 2,
+        "inicioCobertura": "01-01-2024",
+        "finCobertura": "01-01-2025",
+        "planId": null,
+        "onboarding": "en progreso"
       }
-    ]);
+    ];
+
+    const response = await request(app).get('/miembros').expect(200);
+
+    expect(response.body).toEqual(expectedMembers);
   });
 });
