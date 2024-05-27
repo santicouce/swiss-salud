5a5e1abedc5c8b18416ab8258f94d65d9824fd7e
Santiago <santicouce@outlook.com>
2024-05-27 00:35:21 -0300

    borro codigo no necesario

--- api/newrelic.js
+++ api/newrelic.js
@@ -1,5 +1,5 @@
 exports.config = {
-    app_name: ['library-service'],
+    app_name: ['swiss-salud'],
     license_key: '17cf8884cdea088ae37b33799df50d8cFFFFNRAL',
     logging: {
       level: 'info',
